950ba569513e4ce28b985a117e8cf5c83ebd0764
aushfoish <thecakeisalie2897@yandex.ru>
2026-07-24 21:33:21 +0700

    Update vite.config.ts

--- vite.config.ts
+++ vite.config.ts
@@ -1,8 +1,7 @@
 import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
+import react from '@vitejs/react-refresh'
 
-// https://vite.dev/config/
 export default defineConfig({
-  plugins: [react()],
-  base: '/vk-old-react-ts-zustand/',
+  base: '/vk-old-react-ts-zustand/', 
+  plugins: [react()]
 })
