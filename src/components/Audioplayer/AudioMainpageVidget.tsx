77f590cbf0d7bef0447bcf84e5c44f4ce438e447
Roman Alexandrovich <thecakeisalie2897@yandex.ru>
2026-07-25 20:13:09 +0700

    .

--- src/components/Audioplayer/AudioMainpageVidget.tsx
+++ src/components/Audioplayer/AudioMainpageVidget.tsx
@@ -1,11 +1,25 @@
+import { useEffect } from "react"
+import { userMusicFetch } from "../../UserMusicFetch"
 import Mp3AudioHeader from "./Mp3AudioHeader"
 import Mp3AudioList from "./Mp3AudioList"
 
 export const AudioMainpageVidget = () => {
+
+    const {playlist, isLoading, fetchPlaylist} = userMusicFetch()
+
+    useEffect(() => {
+        fetchPlaylist()
+    }, [])
+
     return (
         <div className="mp3-vidget">
-            <Mp3AudioHeader />
-            <Mp3AudioList />
+            { isLoading === false && playlist !== null && (
+            <>
+                <Mp3AudioHeader />
+                <Mp3AudioList />
+            </>
+                
+            )}
         </div>
     )
 }
