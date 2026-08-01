3532127ce2363bf85f2c76fcac6c7a6ecd1839fd
Roman Alexandrovich <thecakeisalie2897@yandex.ru>
2026-08-01 11:13:31 +0700

    Написал скрипт для загрузки аватарок, теперь они аккуратно сжимаются под размер 40x40

--- src/components/AuthorizationModule.tsx
+++ src/components/AuthorizationModule.tsx
@@ -19,24 +19,15 @@ export const AuthorizationModule = (props:AuthorizationModuleProps) => {
     // }
 
     
-    // дефолтные значения при авторизации (страховка на случай если пользователь забыл ввести какие-то данные)
+
     const [username, setUsername] = useState('Меня уронили в детстве и я не вписал имя при авторизации')
     const [userpic, setUserpic] = useState('https://sun9-47.vkuserphoto.ru/s/v1/ig2/rfOjqk1a0iYxtEfAKCjOo4kI1knPODAoOqdFTAb4_0a-vICXkOqqXgbrsLfre6tKt-A7G1EXkprYNnd1uFyyZ27k.jpg?quality=95&as=32x32,40x40&from=bu&u=lex3t8vsnVkGp_wqpU2osc52wlXoJXzLoeZaL0IPAMo&cs=40x0')
 
-
-    // функция авторизации и проверки авторизации (нужны чтобы авторизовать пользователя если его данные есть в хранилище браузера)
     const authorization = userPostsFetch((state) => state.authorization)
-    const authCheck = userPostsFetch((state) => state.authCheck)
-
-    // анонимная авторизация (выставляются специальные заглушки под анонимный профиль из Zustand)
-    const anonymous = userPostsFetch((state) => state.anonymous)
-
-    // функция загрузки фото на сервер, нужна для передачи в оную бинарника аватарки, которую загружает пользователь
-    const uploadAndProceedPicture = userPostsFetch((state) => state.uploadAndProceedPicture)
-
-    // глобальная переменная из Zustand, нужна для обновления локального стейта авторизации
     const userPic = userPostsFetch((state) => state.userPic)
-
+    const authCheck = userPostsFetch((state) => state.authCheck)
+    const anonymous = userPostsFetch((state) => state.anonymous)
+    const uploadAndProceedPicture = userPostsFetch((state) => state.uploadAndProceedPicture)
 
     useEffect(() => {
         const loginData = localStorage.getItem('userdata')
@@ -48,19 +39,17 @@ export const AuthorizationModule = (props:AuthorizationModuleProps) => {
                    
     }, [])
 
-    // передаю userPic из Zustand в компонент авторизации (обновится только после выполнения асихнхронной операции загрузки фото на сервер)
     useEffect(() => {
         if (userPic !== null) {
             setUserpic(userPic)
         }
     }, [userPic])
     
-    // чтец загруженных файлов, выплёвывает ссылку, которая летит в обработчик фото canvas
     const handleFileReader = (e: React.ChangeEvent<HTMLInputElement>) => {
             
                         const files = e.currentTarget.files
                         if (files && files.length > 0) {
-                            
+                            const file = files[0]
                             const reader = new FileReader()
 
                             reader.onloadend = () => {
@@ -69,12 +58,11 @@ export const AuthorizationModule = (props:AuthorizationModuleProps) => {
                                     imageCompression(picToCompress)
                                 }
                             }
-                            
+                            reader.readAsDataURL(file)
                         }
                     
         }
 
-    // холст и функция сжатия загруженных пользовательских фото
     const canvasRef = useRef<HTMLCanvasElement>(null)
     const canvas = canvasRef.current
     const imageCompression = (userpic: string) => {
