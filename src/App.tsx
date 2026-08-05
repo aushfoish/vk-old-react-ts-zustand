9f62a459bcf7098e36873a16aa28a23a04bc1361
Roman Alexandrovich <thecakeisalie2897@yandex.ru>
2026-08-05 19:13:12 +0700

    hotfix

--- src/App.tsx
+++ src/App.tsx
@@ -4,7 +4,7 @@ import AppAside from './components/Navigation/AppAside'
 import AppHeader from './components/AppHeader'
 import UserPage from './components/UserPage'
 import AudioPage from './components/AudioPage'
-import { createHashRouter, RouterProvider, Outlet, Navigate } from 'react-router-dom';
+import { createBrowserRouter, RouterProvider, Outlet, Navigate } from 'react-router-dom';
 import { AudioMainpageVidget } from './components/Audioplayer/AudioMainpageVidget'
 
 
@@ -41,7 +41,7 @@ const MainLayout = () => {
   )
 }
 
-const router = createHashRouter([
+const router = createBrowserRouter([
   {
     path: '/',
     element: <MainLayout />,
