722f110e57e65c5461fa45dca5daf97031a662fd
Roman Alexandrovich <romalkat@yandex.ru>
2026-08-15 22:37:33 +0700

    hotfix: удалил лишнее,внёс мелкие правки по отпимизации

--- src/pages/music/ui/AudioPage.tsx
+++ src/pages/music/ui/AudioPage.tsx
@@ -1,13 +1,12 @@
-
-import { AuthorizationModule } from "@/features/pass-the-authorization/ui/AuthorizationModule";
-import { ModalWindow } from "@/shared/ui/ModalWindow/ModalWindow";
 import { userMusicFetch } from "@/UserMusicFetch";
 import { Mp3AudioList } from "@/widgets/mp3-audio-list";
 import { Mp3AudioHeader } from "@/widgets/mp3-player-header";
 import { useEffect } from "react";
 
 export const AudioPage = () => {
-  const { playlist, isLoading, fetchPlaylist } = userMusicFetch();
+  const playlist = userMusicFetch((state) => state.playlist)
+  const isLoading = userMusicFetch((state) => state.isLoading)
+  const fetchPlaylist = userMusicFetch((state) => state.fetchPlaylist)
 
   useEffect(() => {
     fetchPlaylist();
@@ -17,7 +16,6 @@ export const AudioPage = () => {
     <div className="music-player">
       {isLoading === false && playlist !== null && (
         <>
-          <ModalWindow children={<AuthorizationModule onClose={() => close()}/>} id="auth" label="Авторизация" onCloseModal={() => close()}/>
           <Mp3AudioHeader />
           <Mp3AudioList />
         </>
