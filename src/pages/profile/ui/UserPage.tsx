361e1db4e9e7908738b94fd546fb6b9cbb6a1fd4
Roman Alexandrovich <thecakeisalie2897@yandex.ru>
2026-08-18 11:28:12 +0700

    chore: вернул базовое поведение страницы при загрузке

--- src/pages/profile/ui/UserPage.tsx
+++ src/pages/profile/ui/UserPage.tsx
@@ -1,15 +1,20 @@
-// import { MainPageAuthorization } from "@/widgets/authorization-window/ui/MainPageAuthorization";
-// import { AccountSection } from "./AccountSection";
-// import { AccountTitle } from "@/entities/user";
-// import { useFetchProfile } from "@/entities/user/model/useFetchProfile";
+import { MainPageAuthorization } from "@/widgets/authorization-window/ui/MainPageAuthorization";
+import { AccountSection } from "./AccountSection";
+import { AccountTitle } from "@/entities/user";
+import { useFetchProfile } from "@/entities/user/model/useFetchProfile";
 import { SkeletonMainPage } from "@/shared/ui/SkeletonMainPage";
 
 export const UserPage = () => {
-  // const { isLoading, profile } = useFetchProfile();
+  const { isLoading, profile } = useFetchProfile();
 
+  if (isLoading || !profile) return <SkeletonMainPage />;
   return (
     <>
-      <SkeletonMainPage />
+      <MainPageAuthorization />
+
+      <AccountTitle />
+
+      <AccountSection />
     </>
   );
 };
