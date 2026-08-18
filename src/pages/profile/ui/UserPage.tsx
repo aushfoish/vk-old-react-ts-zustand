3e811c837d35d72125da427b6e6c744589d2c023
Roman Alexandrovich <thecakeisalie2897@yandex.ru>
2026-08-18 11:07:45 +0700

    chore: тест отображения скелетона

--- src/pages/profile/ui/UserPage.tsx
+++ src/pages/profile/ui/UserPage.tsx
@@ -1,20 +1,15 @@
-import { MainPageAuthorization } from "@/widgets/authorization-window/ui/MainPageAuthorization";
-import { AccountSection } from "./AccountSection";
-import { AccountTitle } from "@/entities/user";
-import { useFetchProfile } from "@/entities/user/model/useFetchProfile";
+// import { MainPageAuthorization } from "@/widgets/authorization-window/ui/MainPageAuthorization";
+// import { AccountSection } from "./AccountSection";
+// import { AccountTitle } from "@/entities/user";
+// import { useFetchProfile } from "@/entities/user/model/useFetchProfile";
 import { SkeletonMainPage } from "@/shared/ui/SkeletonMainPage";
 
 export const UserPage = () => {
-  const { isLoading, profile } = useFetchProfile();
+  // const { isLoading, profile } = useFetchProfile();
 
-  if (isLoading || !profile) return <SkeletonMainPage />;
   return (
     <>
-      <MainPageAuthorization />
-
-      <AccountTitle />
-
-      <AccountSection />
+      <SkeletonMainPage />
     </>
   );
 };
