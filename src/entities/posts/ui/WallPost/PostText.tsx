9ac93abdd267136ee02b5a4d621cafb49144d83e
Roman Alexandrovich <romalkat@yandex.ru>
2026-08-19 23:57:03 +0700

    .

--- src/entities/posts/ui/WallPost/PostText.tsx
+++ src/entities/posts/ui/WallPost/PostText.tsx
@@ -9,6 +9,7 @@ export const PostText = (props:PostTextProps) => {
     const {
         text, id
     } = props
+    const lol = 'facepalm'
     return (
         <div className={styles.textContent} id={String(id)}>
                     <p className={styles.userContent}>
