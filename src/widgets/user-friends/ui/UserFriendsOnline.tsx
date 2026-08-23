b7b28bc6c2868fcac33434ed3367609a92557431
Roman Alexandrovich <romalkat@yandex.ru>
2026-08-24 00:45:34 +0700

    perf: добавлена библиотека tanstack query

--- src/widgets/user-friends/ui/UserFriendsOnline.tsx
+++ src/widgets/user-friends/ui/UserFriendsOnline.tsx
@@ -6,7 +6,6 @@ import { useFetchProfile } from "@/entities/user/model/useFetchProfile";
 
 export const UserFriendsOnline = () => {
   const {friendsCount} = useFetchProfile();
-
   return (
     <div className={styles.friendsBlock}>
       <Micro_header children="Друзья" count={`${friendsCount} друзей`} />
