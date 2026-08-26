8a1e03c6880f99843a17d61301e0f07796b1bbd5
Roman Alexandrovich <thecakeisalie2897@yandex.ru>
2026-08-26 15:11:25 +0700

    refactor: добавлена обработка и типизация ошибок в запросы

--- src/shared/lib/file/useFileReader.ts
+++ src/shared/lib/file/useFileReader.ts
@@ -16,25 +16,39 @@ export const handleFileReader = (
       const file = files[0];
 
       if (file.size > maxSize) {
-        e.currentTarget.value = ""
-        resolve(false)
+        e.currentTarget.value = "";
+        resolve(false);
       } else {
         const reader = new FileReader();
-        reader.onloadend = async () => {
+        reader.onload = async () => {
           if (typeof reader.result === "string") {
-            const picToCompress = reader.result;
-            const result = await imageCompression(
-              picToCompress,
-              useScenario,
-              extesnion,
-              width,
-              height,
-            );
-            resolve(result);
+            try {
+              const picToCompress = reader.result;
+              const result = await imageCompression(
+                picToCompress,
+                useScenario,
+                extesnion,
+                width,
+                height,
+              );
+              resolve(result);
+            } catch (error) {
+              console.error(
+                "Ошибка при сжатии изображения:",
+                error instanceof Error,
+              );
+              resolve(false);
+            }
           } else {
             resolve(false);
           }
         };
+
+        reader.onerror = () => {
+          console.error('Ошибка при чтении файла:',
+            reader.error instanceof Error ? reader.error.message : 'не удалось прочитать файл'
+          )
+        }
         reader.readAsDataURL(file);
       }
     }
